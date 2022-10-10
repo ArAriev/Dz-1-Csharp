744a1d083194685e623e3b6f73b4bbbba6cab3a9
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 23:02:05 +0300

    dobavil zadanie008

---
+++ zadanie008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int A = new Random().Next(10, 99);
+int Max = 0;
+int MaxDigit()
+{
+    int FirstDigit = A / 10;
+    int SecondDigit = A % 10;
+    if (FirstDigit > SecondDigit) Max = FirstDigit;
+    else Max = SecondDigit;
+    return Max;
+}
+MaxDigit();
+Console.WriteLine();
+Console.WriteLine($"В числе {A} наибольшая цифра {Max}");
+Console.WriteLine();
