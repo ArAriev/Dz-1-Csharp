c788bbc3182dd888ec058d6b4ac37eb91b0eacf9
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 23:22:01 +0300

    dobavil zadanie009

---
+++ zadanie009/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int Number = new Random().Next(100, 999);
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecDig();
+
+Console.WriteLine();
+Console.WriteLine($"Если удалить из числа {Number} вторую цифру, то останется {NewNumber}");
+Console.WriteLine();
