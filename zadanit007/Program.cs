a861defbe491e4d12088a4455beeaef3c4002204
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 22:49:53 +0300

    dobavil zadanie007

---
+++ zadanit007/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Второй цифрой треззначного числа {number} является {number / 10 % 10} ");
+Console.WriteLine(); 
+
