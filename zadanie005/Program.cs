e0d124e300836981a247efb204e89fd4ff683f9c
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 22:31:22 +0300

    dobavil zadanie005

---
+++ zadanie005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите длину числового ряда: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for ( int i =2; i <= n; i += 2)
+Console.WriteLine(i);
+
