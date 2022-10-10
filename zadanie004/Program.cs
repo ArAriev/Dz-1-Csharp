6c5b2db05deca90056e8ef2a020895d1d1fc997f
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 22:08:56 +0300

    dobavil zadanie004

---
+++ zadanie004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0) 
+Console.WriteLine($"число {n} четное число");
+
+else 
+Console.WriteLine($"число {n} не четное число");
