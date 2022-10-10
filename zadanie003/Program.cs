493083e9161b277f92add910410e63cd110bfd18
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 22:04:28 +0300

    dobavil zadanie003

---
+++ zadanie003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел.
+
+int A = 13;
+int B = 19;
+int C = 26;
+ if (A > B && A > C)
+Console.WriteLine("A"); 
+else if (B > C)
+Console.WriteLine("B"); 
+else
+Console.WriteLine("C"); 
