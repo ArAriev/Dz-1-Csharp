daddfe3049c3b5ca5cff44ddce19a070a8e212b7
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-10 21:40:07 +0300

    reshil zadanie001

---
+++ zadanie001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задание 1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if ( b * b == a )
+Console.WriteLine("Первое число является квадратом второго числа");
+
+else 
+Console.WriteLine("Первое число не является квадратом второго числа");
+
+Console.WriteLine();
